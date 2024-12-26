f49baea2006a2ff9d65fd5babae0282f9e75b5e2
Alpha Bah <alphabah2395@gmail.com>
2024-12-25 21:52:14 -0500

    final

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Spotify-Mood-Frontend/',
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
